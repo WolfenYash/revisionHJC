0603b3f42790732c41cf428c60ed8aab6ff76418
WolfenYash <100674003+WolfenYash@users.noreply.github.com>
2025-06-24 21:24:33 +0530

    Add files via upload

---
+++ HTML-CSS/script.js
@@ -0,0 +1,13 @@
+// let mydiv =  document.body.querySelector("div");
+
+let mybtn = document.createElement("button");
+mybtn.innerHTML = "Click Me!";
+mybtn.style.backgroundColor = "red";
+mybtn.style.color = "white";
+mybtn.style.padding = "10px";
+
+document.body.prepend(mybtn);
+// document.querySelector("body").prepend(mybtn);
+
+let mypara = document.querySelectorAll(".paragraph");
+
